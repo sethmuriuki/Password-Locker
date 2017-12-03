5f4f21759d3cbe0f1f576edfd2180128712d30a3
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-12-03 09:16:32 +0300

    run second test to save user objects plus create save method

--- user-test.py
+++ user-test.py
@@ -16,20 +16,29 @@ class TestUser(unittest.TestCase):
 
     def setUp(self):
 
-            '''
-            method to run before each test case
-            '''
+        '''
+        method to run before each test case
+        '''
 
-            self.new_user = User("seth","password")
+        self.new_user = User("seth","password")
 
     def test_init_(self):
 
-            '''
-            test case to if the User object is initialized properly
-            '''
+        '''
+        test case to if the User object is initialized properly
+        '''
 
-            self.assertEqual(self.new_user.user_name,"seth")
-            self.assertEqual(self.new_user.password,"password")
+        self.assertEqual(self.new_user.user_name,"seth")
+        self.assertEqual(self.new_user.password,"password")
+
+    def test_save_user(self):
+
+        '''
+        test case to test if user objects have been saved into user_list
+        '''
+
+        self.new_user.save_user() #save new user
+        self.assertEqual(len(User.user_list),1)
 
 
 
--- user.py
+++ user.py
@@ -4,6 +4,16 @@ class User:
     class that generates new instance of user
     '''
 
+    user_list = []
+
     def __init__(self,user_name,password):
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+
+        '''
+        save_user method saves a new user objects to the user_list
+        '''
+
+        User.user_list.append(self)
